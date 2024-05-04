1e6e2675b7c7fddd61b29256e1467cb295339df3
YonasTewabe <yonastewabe21@gmail.com>
2024-05-04 14:58:23 -0500

    vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JobsModule } from './jobs/jobs.module';
-import { ConfigModule } from '@nestjs/config';
-import {config} from './orm.config'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import {config} from './orm.config'
 import { ApplicationModule } from './application/application.module';
 import { ProfileModule } from './profile/profile.module';
 import { MulterModule } from '@nestjs/platform-express';
@@ -22,7 +22,21 @@ import { diskStorage } from 'multer';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(config),
+    // TypeOrmModule.forRoot(config),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('POSTGRES_URL'),
+        // host: configService.get<string>('DB_HOST'),
+        // port: configService.get<number>('DB_PORT'),
+        // username: configService.get<string>('DB_USERNAME'),
+        // password: configService.get<string>('DB_PASSWORD'),
+        // database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     JobsModule,
     ApplicationModule,
     ProfileModule
--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,13 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  url: 'postgres://default:3eQibNS5tLgD@ep-billowing-queen-a45bomrk-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require',
-  // username: 'postgres',
-  // password: '1234',
-  // port: 5432,
-  // host: '127.0.0.1',
-  // database: 'capstoneProject',
+  username: 'postgres',
+  password: '1234',
+  port: 5432,
+  host: '127.0.0.1',
+  database: 'capstoneProject',
   synchronize: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
-
 };
