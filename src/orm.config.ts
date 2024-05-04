581a9257ebfb3dcfe6415508e235bfefaab48c39
YonasTewabe <yonastewabe21@gmail.com>
2024-05-04 13:54:27 -0500

    vercel

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,11 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  username: 'postgres',
-  password: '1234',
-  port: 5432,
-  host: '127.0.0.1',
-  database: 'capstoneProject',
+  url: 'postgres://default:3eQibNS5tLgD@ep-billowing-queen-a45bomrk-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require',
+  // username: 'postgres',
+  // password: '1234',
+  // port: 5432,
+  // host: '127.0.0.1',
+  // database: 'capstoneProject',
   synchronize: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
+
 };
