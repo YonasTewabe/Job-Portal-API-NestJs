5ba4c9f266eecc92c03bfc897bcf07b44ee83510
brooktewabe <brooktewabe00@gmail.com>
2024-07-20 10:57:52 +0300

    'docker'

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -2,10 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
   username: 'postgres',
-  password: '1234',
+  password: 'postgres',
+  // password: '1234',
   port: 5432,
-  host: '127.0.0.1',
-  database: 'capstoneProject',
+  host: 'db',
+  database: 'capstone',
+  // host: '127.0.0.1',
+  // database: 'capstoneProject',
   synchronize: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
 };
